5be38352357f65e4621384531b0e2bb501dac686
pankaspe <mez.tnt@gmail.com>
2023-09-29 13:51:06 +0200

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
           /* {to: '/blog', label: 'Blog', position: 'left'}, */
           {to: '/about', label: 'il Progetto', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/pankaspe/rusty-guardians',
             label: 'GitHub',
             position: 'right',
           },
