8901d998d64903e7b6b987c8f5b706fee6d5b4ad
pankaspe <mez.tnt@gmail.com>
2023-10-15 22:26:33 +0200

    fix some issue

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,14 +1,30 @@
-// src/theme/Root.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CookieBanner from '../components/CookieBanner';
 import { inject } from '@vercel/analytics';
 
 const Root = ({ children }) => {
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const loadAnalyticsScript = async () => {
+      try {
+        await inject();
+      } catch (err) {
+        setError(err);
+      }
+    };
+
+    loadAnalyticsScript();
+  }, []); // Assicurati di passare un array vuoto come dipendenza per eseguire l'effetto solo una volta
+
   return (
     <>
       <CookieBanner />
-      {children}
-      {inject()}
+      {error ? (
+        <div>Errore durante il caricamento dello script di analisi: {error.message}</div>
+      ) : (
+        children
+      )}
     </>
   );
 };
