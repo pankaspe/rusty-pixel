c90b36b34a61e3d32917bdabf993884ba705c828
pankaspe <mez.tnt@gmail.com>
2023-10-15 22:10:36 +0200

    fix some issue

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,14 +1,14 @@
 // src/theme/Root.js
 import React from 'react';
 import CookieBanner from '../components/CookieBanner';
-import { Analytics } from '@vercel/analytics/react';
+import { inject } from '@vercel/analytics';
 
 const Root = ({ children }) => {
   return (
     <>
       <CookieBanner />
       {children}
-      <Analytics />
+      {inject()}
     </>
   );
 };
